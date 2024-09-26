0ec60b5d167dd58e64fa76f880ba8e7de31dc97d
Patriciaan <70260484+Patriciaan@users.noreply.github.com>
2024-09-26 12:54:32 +0800

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDK9Uy817aBJYd1OO1UPu9VDaCMUHp-Ibg",
+    authDomain: "capstone-71317.firebaseapp.com",
+    databaseURL: "https://capstone-71317-default-rtdb.firebaseio.com",
+    projectId: "capstone-71317",
+    storageBucket: "capstone-71317.appspot.com",
+    messagingSenderId: "352296370394",
+    appId: "1:352296370394:web:202168464fdece9889a5e3",
+    measurementId: "G-JXH1VMSJXN"
+  };
+  
